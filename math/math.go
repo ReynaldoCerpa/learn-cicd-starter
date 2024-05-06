6d8feb095990b55f78f42de472cc7a8633367c74
ReynaldoCerpa <reynaldocerpa07@gmail.com>
2024-05-06 11:49:56 -0700

    add broken code

---
+++ math/math.go
@@ -0,0 +1,11 @@
+package math
+
+// Add is our function that sums two integers
+func Add(x, y int) (res int) {
+	return x + 
+}
+
+// Subtract subtracts two integers
+func Subtract(x, y int) (res int) {
+	return x - y
+}
